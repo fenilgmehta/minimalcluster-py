4d2b27f23e5c6e6b9a4086ab85b440521beffec4
Fenil Mehta <fenilgmehta@gmail.com>
2019-11-15 11:13:18 +0530

    Update __init__.py to include new functions for import

--- minimalcluster/__init__.py
+++ minimalcluster/__init__.py
@@ -1,11 +1,10 @@
-
-__all__ = ['MasterNode', "WorkerNode"]
-
+__all__ = ['MasterNode', "WorkerNode", 'set_debug_level', 'get_debug_level']
 
 import sys
+
 if sys.version_info.major == 3:
     from .master_node import MasterNode
-    from .worker_node import WorkerNode
+    from .worker_node import WorkerNode, set_debug_level, get_debug_level
 else:
     from master_node import MasterNode
-    from worker_node import WorkerNode
+    from worker_node import WorkerNode, set_debug_level, get_debug_level
