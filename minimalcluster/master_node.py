c774747ea0312e3e945a3a6ffabf39287ff5880d
Fenil Mehta <fenilgmehta@gmail.com>
2019-11-15 11:26:28 +0530

    Reformat file, Fix doc strings, Fix spelling mistakes, Change == to `is`

--- minimalcluster/master_node.py
+++ minimalcluster/master_node.py
@@ -1,27 +1,32 @@
-from multiprocessing.managers import SyncManager, DictProxy
-from multiprocessing import Process, cpu_count
-import os, signal, sys, time, datetime, random, string, inspect
+import datetime
+import os
+import random
+import signal
+import string
+import sys
+import time
 from functools import partial
-from types import FunctionType
+from multiprocessing import Process, cpu_count
+from multiprocessing.managers import SyncManager, DictProxy
 from socket import getfqdn
+from types import FunctionType
+
 if sys.version_info.major == 3:
     from queue import Queue as _Queue
 else:
     from Queue import Queue as _Queue
 
-
-
 __all__ = ['MasterNode']
-    
 
 
 # Make Queue.Queue pickleable
 # Ref: https://stackoverflow.com/questions/25631266/cant-pickle-class-main-jobqueuemanager
 class Queue(_Queue):
-    """ A picklable queue. """   
+    """ A picklable queue. """
+
     def __getstate__(self):
         # Only pickle the state we care about
-        return (self.maxsize, self.queue, self.unfinished_tasks)
+        return self.maxsize, self.queue, self.unfinished_tasks
 
     def __setstate__(self, state):
         # Re-initialize the object, then overwrite the default state with
@@ -31,10 +36,10 @@ class Queue(_Queue):
         self.queue = state[1]
         self.unfinished_tasks = state[2]
 
+
 # prepare for using functools.partial()
 def get_fun(fun):
     return fun
-
 
 
 class JobQueueManager(SyncManager):
@@ -52,10 +57,10 @@ def start_worker_in_background(HOST, PORT, AUTHKEY, nprocs, quiet):
     worker.join_cluster()
 
 
-class MasterNode():
+class MasterNode:
 
-    def __init__(self, HOST = '127.0.0.1', PORT = 8888, AUTHKEY = None, chunksize = 50):
-        '''
+    def __init__(self, HOST='127.0.0.1', PORT=8888, AUTHKEY=None, chunksize=50):
+        """
         Method to initiate a master node object.
 
         HOST: the hostname or IP address to use
@@ -64,12 +69,12 @@ class MasterNode():
                  If None is given, a random string will be given
         chunksize: The numbers are split into chunks. Each chunk is pushed into the job queue.
                    Here the size of each chunk if specified.
-        '''
+        """
 
         # Check & process AUTHKEY
-        # to [1] ensure compatilibity between Py 2 and 3; [2] to allow both string and byte string for AUTHKEY input.
-        assert type(AUTHKEY) in [str, bytes] or AUTHKEY is None, "AUTKEY must be either one among string, byte string, and None (a random AUTHKEY will be generated if None is given)."
-        if AUTHKEY != None and type(AUTHKEY) == str:
+        # to [1] ensure compatibility between Py 2 and 3; [2] to allow both string and byte string for AUTHKEY input.
+        assert type(AUTHKEY) in [str, bytes] or AUTHKEY is None, \
+            "AUTKEY must be either one among string, byte string, and None (a random AUTHKEY will be generated if None is given)."
             AUTHKEY = AUTHKEY.encode()
 
         self.HOST = HOST
@@ -84,15 +89,15 @@ class MasterNode():
 
 
     def join_as_worker(self):
-        '''
+        """
         This method helps start the master node as a worker node as well
-        '''
+        """
         if self.as_worker:
             print("[WARNING] This node has already joined the cluster as a worker node.")
         else:
-            self.process_as_worker = Process(target = start_worker_in_background, args=(self.HOST, self.PORT, self.AUTHKEY, cpu_count(), True, ))
+            self.process_as_worker = Process(target=start_worker_in_background, args=(self.HOST, self.PORT, self.AUTHKEY, cpu_count(), True,))
             self.process_as_worker.start()
-            
+
             # waiting for the master node joining the cluster as a worker
             while self.master_fqdn not in [w[0] for w in self.list_workers()]:
                 pass
@@ -100,16 +105,16 @@ class MasterNode():
             self.pid_as_worker_on_master = [w for w in self.list_workers() if w[0] == self.master_fqdn][0][2]
             self.as_worker = True
             print("[INFO] Current node has joined the cluster as a Worker Node (using {} processors; Process ID: {}).".format(cpu_count(), self.process_as_worker.pid))
-        
-    def start_master_server(self, if_join_as_worker = True):
+
+    def start_master_server(self, if_join_as_worker=True):
         """
         Method to create a manager as the master node.
-        
+
         Arguments:
         if_join_as_worker: Boolen.
                         If True, the master node will also join the cluster as worker node. It will automatically run in background.
                         If False, users need to explicitly configure if they want the master node to work as worker node too.
-                        The default value is True.                             
+                        The default value is True.
         """
         self.job_q = Queue()
         self.result_q = Queue()
@@ -117,8 +122,8 @@ class MasterNode():
         self.get_envir = Queue()
         self.target_function = Queue()
         self.raw_queue_of_worker_list = Queue()
-        self.raw_dict_of_job_history = dict() # a queue to store the history of job assignment
-        
+        self.raw_dict_of_job_history = dict()  # a queue to store the history of job assignment
+
         # Return synchronized proxies for the actual Queue objects.
         # Note that for "callable=", we don't use `lambda` which is commonly used in multiprocessing examples.
         # Instead, we use `partial()` to wrapper one more time.
@@ -128,10 +133,10 @@ class MasterNode():
         JobQueueManager.register('get_job_q', callable=partial(get_fun, self.job_q))
         JobQueueManager.register('get_result_q', callable=partial(get_fun, self.result_q))
         JobQueueManager.register('get_error_q', callable=partial(get_fun, self.error_q))
-        JobQueueManager.register('get_envir', callable = partial(get_fun, self.get_envir))
-        JobQueueManager.register('target_function', callable = partial(get_fun, self.target_function))
-        JobQueueManager.register('queue_of_worker_list', callable = partial(get_fun, self.raw_queue_of_worker_list))
-        JobQueueManager.register('dict_of_job_history', callable = partial(get_fun, self.raw_dict_of_job_history), proxytype=DictProxy)
+        JobQueueManager.register('get_envir', callable=partial(get_fun, self.get_envir))
+        JobQueueManager.register('target_function', callable=partial(get_fun, self.target_function))
+        JobQueueManager.register('queue_of_worker_list', callable=partial(get_fun, self.raw_queue_of_worker_list))
+        JobQueueManager.register('dict_of_job_history', callable=partial(get_fun, self.raw_dict_of_job_history), proxytype=DictProxy)
 
         self.manager = JobQueueManager(address=(self.HOST, self.PORT), authkey=self.AUTHKEY)
         self.manager.start()
@@ -145,18 +150,18 @@ class MasterNode():
         self.share_target_fun = self.manager.target_function()
         self.queue_of_worker_list = self.manager.queue_of_worker_list()
         self.dict_of_job_history = self.manager.dict_of_job_history()
-        
+
         if if_join_as_worker:
             self.join_as_worker()
 
     def stop_as_worker(self):
-        '''
+        """
         Given the master node can also join the cluster as a worker, we also need to have a method to stop it as a worker node (which may be necessary in some cases).
         This method serves this purpose.
 
         Given the worker node will start a separate process for heartbeat purpose.
         We need to shutdown the heartbeat process separately.
-        '''
+        """
         try:
             os.kill(self.pid_as_worker_on_master, signal.SIGTERM)
             self.pid_as_worker_on_master = None
@@ -168,48 +173,44 @@ class MasterNode():
             print("[INFO] The master node has stopped working as a worker node.")
 
     def list_workers(self):
-        '''
+        """
         Return a list of connected worker nodes.
         Each element of this list is:
             (hostname of worker node,
              # of available cores,
              pid of heartbeat process on the worker node,
              if the worker node is working on any work load currently (1:Yes, 0:No))
-        '''
-
+        """
         # STEP-1: an element will be PUT into the queue "self.queue_of_worker_list"
         # STEP-2: worker nodes will watch on this queue and attach their information into this queue too
         # STEP-3: this function will collect the elements from the queue and return the list of workers node who responded
 
-        self.queue_of_worker_list.put(".") # trigger worker nodes to contact master node to show their "heartbeat"
-        time.sleep(0.3) # Allow some time for collecting "heartbeat"
-        
+        self.queue_of_worker_list.put(".")  # trigger worker nodes to contact master node to show their "heartbeat"
+        time.sleep(0.3)  # Allow some time for collecting "heartbeat"
+
         worker_list = []
         while not self.queue_of_worker_list.empty():
             worker_list.append(self.queue_of_worker_list.get())
 
         return list(set([w for w in worker_list if w != "."]))
-        
 
-    def load_envir(self, source, from_file = True):
+    def load_envir(self, source, from_file=True):
         if from_file:
             with open(source, 'r') as f:
                 self.envir_statements = "".join(f.readlines())
         else:
-            self.envir_statements = source   
-        
+            self.envir_statements = source
+
     def register_target_function(self, fun_name):
         self.target_fun = fun_name
-        
+
     def load_args(self, args):
-        '''
+        """
         args should be a list
-        '''
+        """
         self.args_to_share_to_workers = args
 
-
     def __check_target_function(self):
-
         try:
             exec(self.envir_statements)
         except:
@@ -221,13 +222,11 @@ class MasterNode():
         else:
             return False
 
-
     def execute(self):
-
         # Ensure the error queue is empty
         clear_queue(self.shared_error_q)
 
-        if self.target_fun == None:
+        if self.target_fun is None:
             print("[ERROR] Target function is not registered yet.")
         elif not self.__check_target_function():
             print("[ERROR] The target function registered (`{}`) can't be built with the given environment statements.".format(self.target_fun))
@@ -238,7 +237,6 @@ class MasterNode():
         else:
             print("[{}] Assigning jobs to worker nodes.".format(str(datetime.datetime.now())))
 
-            
             self.share_envir.put(self.envir_statements)
 
             self.share_target_fun.put(self.target_fun)
@@ -246,7 +244,7 @@ class MasterNode():
             # The numbers are split into chunks. Each chunk is pushed into the job queue
             for i in range(0, len(self.args_to_share_to_workers), self.chunksize):
                 self.shared_job_q.put((i, self.args_to_share_to_workers[i:(i + self.chunksize)]))
-            
+
             # Wait until all results are ready in shared_result_q
             numresults = 0
             resultdict = {}
