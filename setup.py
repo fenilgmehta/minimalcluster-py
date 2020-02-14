efecd42ef544dc484cda1f47c74c24e1b94b038a
Fenil Mehta <fenilgmehta@gmail.com>
2020-02-14 12:12:13 +0530

    Update lib version 0.1.0.dev14 ---> 0.1.1.dev15

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst')) as f:
 
 setup(
     name="minimalcluster",
-    version="0.1.0.dev14",
+    version="0.1.1.dev15",
     description='A minimal cluster computing framework',
     long_description=long_description,
     url='https://github.com/XD-DENG/minimalcluster-py',
